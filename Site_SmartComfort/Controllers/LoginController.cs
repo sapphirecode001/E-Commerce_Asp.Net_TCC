507e93268e0cdfd66e772f278a025b7633eb8784
3Amodular <3mod@bglab.local>
2024-09-06 14:42:03 -0300

    adicionando ao git da empresa

---
+++ Site_SmartComfort/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Site_SmartComfort.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Site_SmartComfort/Controllers/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Site_SmartComfort.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Produto()
+        {
+            return View();
+        }
+
+        public IActionResult IndexProdutos()
+        {
+            return View();
+        }
+    }
+}
